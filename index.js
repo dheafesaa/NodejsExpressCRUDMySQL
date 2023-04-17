bcece037c88aa69055d3fab0a6b7d2def2211393
depsaaa0823 <depsaaa0823@gmail.com>
2023-04-17 16:54:27 +0700

    Update Package

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const multer = require("multer");
 const path = require("path");
+var cors = require("cors");
 // set body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // script upload
 
